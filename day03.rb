240758caeeb002dc868c167bc632f9cfe2d5ef69
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2024-12-05 17:36:32 +0100

    :sickos:

--- day03.rb
+++ day03.rb
@@ -1,15 +1,4 @@
 #!/usr/bin/ruby -w
 
-# input = ""
-# # read from stdin
-# while line = gets
-#     input += line.chomp
-# end
-
-# mult_instr = input.scan(/mul\(\d{1,3},\d{1,3}\)/)
-
-# puts mult_instr.map { |instr| instr.scan(/\d{1,3}/).map(&:to_i).inject(:*) }.inject(0, :+)
-
 puts $stdin.read.scan(/mul\(\d{1,3},\d{1,3}\)/).map { |instr| instr.scan(/\d{1,3}/).map(&:to_i).inject(:*) }.inject(0, :+)
 
-#$stdin.read.scan(/mul\(\d{1,3},\d{1,3}\)/).map { |instr| instr.scan(/\d{1,3}/).map(&:to_i).inject(:*) }.inject(0, :+)
