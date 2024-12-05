96d5729ab1082c6cf761a39ec01f082e3fd6b81d
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2024-12-05 17:38:30 +0100

    :sickos:

--- day03.rb
+++ day03.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/ruby -w
 
-puts $stdin.read.scan(/mul\(\d{1,3},\d{1,3}\)/).map { |instr| instr.scan(/\d{1,3}/).map(&:to_i).inject(:*) }.inject(0, :+)
+puts $stdin.read.scan(/mul\(\d{1,3},\d{1,3}\)/).map { |instr| instr.scan(/\d{1,3}/).map(&:to_i).inject(:*) }.inject(:+)
 
