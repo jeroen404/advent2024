834b530b8a04a37644621ac8fafa9b00922283fa
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2024-12-11 23:33:24 +0100

    shorter

--- day11.rb
+++ day11.rb
@@ -18,7 +18,6 @@ def split_number_in_two(number,nb_digits)
     return left_half, right_half
 end
 
-
 def blink(number,blinks)
     if blinks == $total_blinks
         return 1
@@ -28,25 +27,18 @@ def blink(number,blinks)
     end
     if number == 0
         result = blink(1,blinks + 1)
-        $blink_cache[[number,blinks]] = result
-        return result
     else 
         nb_digits = nb_digits(number)
         if nb_digits.even?
             left_half, right_half = split_number_in_two(number,nb_digits)
-            left_result = blink(left_half,blinks + 1)
-            right_result = blink(right_half,blinks + 1)
-            result = left_result + right_result
-            $blink_cache[[number,blinks]] = result
-            return result
+             result = blink(left_half,blinks + 1) + blink(right_half,blinks + 1)
         else
             result = blink(number * 2024,blinks + 1)
-            $blink_cache[[number,blinks]] = result
-            return result
         end
     end
+    $blink_cache[[number,blinks]] = result
+    return result
 end
-
 
 puts $stones.map { |stone| blink(stone,0) }.inject(:+)
 
