ed67d4e06ab836a2e9aa3599a4ef9ab8397c02cd
Jeroen Coekaerts <jeroen@dnxnetwork.lu>
2024-12-22 23:30:46 +0100

    day22 shorter

--- day22.rb
+++ day22.rb
@@ -1,38 +1,13 @@
 #!/usr/bin/ruby -w
 
-
-def mix(a,b)
-    return a ^ b
-end
-
-def prune(a)
-    return a % 16777216 # 2^24
-    #return a & 0xFFFFFF
+def prune_mix(a,b)
+    return (a ^ b) % 16777216
 end
 
 def next_secret(a)
-    b = a * 64   # 2^6
-    #b = a << 6
-    a = mix(a,b)
-    a = prune(a)
-
-    c = a / 32  # 2^5
-    #c = a >> 5
-    a = mix(a,c)
-    a = prune(a)
-
-    d = a * 2048 # 2^11
-    #d = a << 11
-    a = mix(a,d)
-    a = prune(a)
-
-    return a
-end
-
-def part1(a,times)
-    times.times do
-        a = next_secret(a)
-    end
+    a = prune_mix(a,a * 64)
+    a = prune_mix(a,a / 32)
+    a = prune_mix(a,a * 2048)
     return a
 end
 
@@ -43,33 +18,20 @@ while line = gets
 end
 
 #part 1
-puts numbers.map { |n| part1(n,2000) }.inject(:+)
+puts numbers.map { |n| 2000.times.inject(n) { |a| next_secret(a) } }.sum
+#puts numbers.map { |n| part1(n,2000) }.inject(:+)
 
-prices = []
-numbers.each do |n|
-    prices_for_this_monkey = []
-    2000.times do
-        n = next_secret(n)
-        prices_for_this_monkey << n % 10
-    end
-    prices << prices_for_this_monkey
-end
+prices = numbers.map { |n| Array.new(2000) { n = next_secret(n); n % 10 } }
 
-price_changes = []
-numbers.length.times do |i|
-    price_changes_for_this_monkey = []
-    prices[i].each_cons(2) do |a,b|
-        price_changes_for_this_monkey << b - a
-    end
-    price_changes << price_changes_for_this_monkey
-end
+#price_changes = prices.map { |p| p.each_cons(2).map { |a,b| b - a } }
 
 sequence_price_hash = Hash.new(0)
-numbers.length.times do |i|
+
+prices.each_with_index do |monkey_prices,i|
     sequences_for_this_monkey = {}
-    price_changes[i].each_cons(4).with_index do |changes,j|
-        if !sequences_for_this_monkey[changes]
-            sequence_price_hash[changes] += prices[i][j+4]
+    monkey_prices.each_cons(2).map { |a, b| b - a}.each_cons(4).with_index do |changes,j|
+        unless sequences_for_this_monkey.has_key?(changes)
+            sequence_price_hash[changes] += monkey_prices[j+4]
             sequences_for_this_monkey[changes] = true
         end
     end
